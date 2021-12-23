b623221774f3c2e19270caf6cd4cffac24c10d0b
KateLis <qualitysw100@gmail.com>
2021-12-23 23:47:51 +0300

    Task 1

---
+++ src/main/java/TestOneChrome.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestOneChrome {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,10);
+    }
+    @Test
+    public void myFirstTest(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Chromedriver\\chromedriver_win32\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("http:/www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+    }
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+
+}
