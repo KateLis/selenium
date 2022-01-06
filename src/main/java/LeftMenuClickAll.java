4c3ae9bf8472e66b67819be30e18b5c335892422
KateLis <qualitysw100@gmail.com>
2022-01-06 22:00:07 +0300

    Task 6. Click all left menu steps

---
+++ src/main/java/LeftMenuClickAll.java
@@ -0,0 +1,43 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class LeftMenuClickAll {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void clickAndCheckLeftMenu() throws InterruptedException {
+        driver.get("http://localhost:8090/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("remember_me")).click();
+        driver.findElement(By.name("login")).click();
+
+        for(int i = 0; i<driver.findElements(By.cssSelector("#box-apps-menu li")).size(); i++){
+            driver.findElements(By.cssSelector("#box-apps-menu li")).get(i).click();
+        }
+
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+
+}
